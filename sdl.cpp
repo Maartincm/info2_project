eae19acf5e3b0210a575d4a2619676fe1bd9353e
Martin Nicolas Cuesta <cuesta.martin.n@hotmail.com>
2017-10-03 23:37:45 -0300

    [MOD] Cleanup the code and comment it (Save BackUp)

--- sdl.cpp
+++ sdl.cpp
@@ -15,16 +15,12 @@ class canvas
         int cur_pix_Y;
         int width;
         int height;
-        int x = 0;
-        int y = 0;
-        SDL_Texture * texture;
+        bool leftMouseButtonDown = false;
+        bool rightMouseButtonDown = false;
         SDL_Renderer * renderer;
         SDL_Window * window;
         SDL_Event event;
-        Uint32 * pixels;
-        bool leftMouseButtonDown = false;
-        bool rightMouseButtonDown = false;
-        bool quit = false;
+        SDL_bool done = SDL_FALSE;
 
     public:
         canvas(int w, int h);
@@ -42,141 +38,87 @@ canvas::canvas (int w, int h)
 void canvas::create_window()
 {
 
+    // Start the SDL widget
     SDL_Init(SDL_INIT_VIDEO);
-
-    // window = SDL_CreateWindow("SDL2 Pixel Drawing",
-    //     SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, 0);
-    //
-    // renderer = SDL_CreateRenderer(window, -1, 0);
     
+    // Draw the main window and instanciate the cursor
     SDL_CreateWindowAndRenderer(width, height, 0, &window, &renderer);
+
+    // Set the color to Black, render that color and show it
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
     SDL_RenderClear(renderer);
-    // texture = SDL_CreateTexture(renderer,
-    //     SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STATIC, width, height);
-    // pixels = new Uint32[width * height];
 
-    // memset(pixels, 255, width * height * sizeof(Uint32));
+    SDL_SetWindowTitle(window, "Magic Drawing");
+    SDL_RenderPresent(renderer);
 
 }
 
 void canvas::_draw()
 {
-    while (!quit)
+    // Keep thw window open until it's explicitly closed
+    while (!done)
     {
-        // SDL_UpdateTexture(texture, NULL, pixels, width * sizeof(Uint32));
-        
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
-
+        // Wait for something to happen, then take the correct action
         SDL_WaitEvent(&event);
 
         switch (event.type)
         {
-            case SDL_KEYDOWN:
-                switch (event.key.keysym.sym)
-                {
-                    case SDLK_LEFT:  x--; break;
-                    case SDLK_RIGHT: x++; break;
-                    case SDLK_UP:    y--; break;
-                    case SDLK_DOWN:  y++; break;
-                }
+            case SDL_QUIT:
+                done = SDL_TRUE;
                 break;
 
-            case SDL_QUIT:
-                quit = true;
+            case SDL_MOUSEMOTION:
+                if (leftMouseButtonDown)
+                {
+                    if (last_pix_X == -1)
+                    {
+                        SDL_GetMouseState(&last_pix_X, &last_pix_Y);
+                    }
+                    else
+                    {
+                        // Save current pointer position
+                        SDL_GetMouseState(&cur_pix_X, &cur_pix_Y);
+                        // Set the cursor color to white, in order to contrast with the background
+                        SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+
+                        // Draw line from last point to current point and refresh the renderer
+                        SDL_RenderDrawLine(renderer, last_pix_X, last_pix_Y,
+                                                     cur_pix_X, cur_pix_Y);
+                        SDL_RenderPresent(renderer);
+                        last_pix_X = cur_pix_X;
+                        last_pix_Y = cur_pix_Y;
+                    }
+                }
                 break;
 
             case SDL_MOUSEBUTTONUP:
                 if (event.button.button == SDL_BUTTON_LEFT)
                     leftMouseButtonDown = false;
-                    if (event.button.button == SDL_BUTTON_RIGHT)
-                        rightMouseButtonDown = false;
+                if (event.button.button == SDL_BUTTON_RIGHT)
+                    rightMouseButtonDown = false;
                 break;
 
             case SDL_MOUSEBUTTONDOWN:
                 if (event.button.button == SDL_BUTTON_LEFT)
                     leftMouseButtonDown = true;
                 if (event.button.button == SDL_BUTTON_RIGHT)
-                    rightMouseButtonDown = true;
-
-            case SDL_MOUSEMOTION:
-                int mouseX = event.motion.x;
-                int mouseY = event.motion.y;
-                std::stringstream ss;
-                ss << "X: " << mouseX << " Y: " << mouseY;
-
-                SDL_SetWindowTitle(window, ss.str().c_str());
-              
-                if (leftMouseButtonDown)
                 {
-                    if (last_pix_X == -1)
-                    {
-                        SDL_GetMouseState(&last_pix_X, &last_pix_Y);
-                        // cout << "\n" << last_pix_X << "\t" << last_pix_Y;
-                    }
-                    else{
-                        SDL_GetMouseState(&cur_pix_X, &cur_pix_Y);
-
-                        // cout << "\n" << last_pix_X << "\t" << last_pix_Y;
-                        // cout << "\n" << cur_pix_X << "\t" << cur_pix_Y;
-                        // cout << "\n\n";
-                        SDL_RenderDrawLine(renderer, last_pix_X, last_pix_Y,
-                                                     cur_pix_X, cur_pix_Y);
-                        last_pix_X = cur_pix_X;
-                        last_pix_Y = cur_pix_Y;
-                    }
-
-                    // pixels[mouseY * width + mouseX+2] = 0;                    
-                    // pixels[mouseY * width + mouseX+1] = 0;
-                    // pixels[mouseY * width + mouseX] = 0;
-                    // pixels[mouseY * width + mouseX-1] = 0;
-                    // pixels[mouseY * width + mouseX-2] = 0;
-                    // pixels[mouseY * width + width + mouseX+2] = 0;
-                    // pixels[mouseY * width + width + mouseX+1] = 0;
-                    // pixels[mouseY * width + width + mouseX] = 0;
-                    // pixels[mouseY * width + width + mouseX-1] = 0;
-                    // pixels[mouseY * width + width + mouseX-2] = 0;
-                    // pixels[mouseY * width - width + mouseX+2] = 0;  
-                    // pixels[mouseY * width - width + mouseX+1] = 0;
-                    // pixels[mouseY * width - width + mouseX] = 0;
-                    // pixels[mouseY * width - width + mouseX-1] = 0;
-                    // pixels[mouseY * width - width + mouseX-2] = 0;
-                    // //pixels[mouseY * width + width * 2 + mouseX+2] = 0;
-                    // pixels[mouseY * width + width * 2 + mouseX+1] = 0;
-                    // pixels[mouseY * width + width * 2 + mouseX] = 0;
-                    // pixels[mouseY * width + width * 2 + mouseX-1] = 0;
-                    // //pixels[mouseY * width + width * 2 + mouseX-2] = 0;
-                    // //pixels[mouseY * width - width * 2 + mouseX+2] = 0;  
-                    // pixels[mouseY * width - width * 2 + mouseX+1] = 0;
-                    // pixels[mouseY * width - width * 2 + mouseX] = 0;
-                    // pixels[mouseY * width - width * 2 + mouseX-1] = 0;
-                    // //pixels[mouseY * width - width * 2 + mouseX-2] = 0;
-                }
-                  if (rightMouseButtonDown)
-                {
+                    // Color to black, and refresh to erase all the window
                     SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
                     SDL_RenderClear(renderer);
-                     
-                 // memset(pixels, 255, width * height * sizeof(Uint32));
-                /*	
-                    int mouseX = event.motion.x;
-                    int mouseY = event.motion.y;
-                    pixels[mouseY * 640 + mouseX+2] = 1;
-                    pixels[mouseY * 640 + mouseX+1] =1;
-                    pixels[mouseY * 640 + mouseX] = 1;
-                */}
+                    SDL_RenderPresent(renderer);
+                    // Keep drawing in white afterwards
+                    SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+                    rightMouseButtonDown = true;
+                }
                 break;
         }
-
-        // SDL_RenderCopy(renderer, texture, NULL, NULL);
-        SDL_RenderPresent(renderer);
     }
 }
 
 canvas::~canvas()
 {
-    // delete[] pixels;
-    // SDL_DestroyTexture(texture);
+    // Free all the used memory
     SDL_DestroyRenderer(renderer);
 
     SDL_DestroyWindow(window);
@@ -185,8 +127,8 @@ canvas::~canvas()
 
 int main(int argc, char ** argv)
 {
-    int width = 1366;
-    int height = 768;
+    int width = 1360;
+    int height = 710;
 
     canvas win(width, height);
     win.create_window();
